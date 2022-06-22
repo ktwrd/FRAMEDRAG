b48c209cc964bad9f324a31613de8122e7bbc68e
kate <kate@dariox.club>
2022-06-22 20:40:03 +0800

    Add class StatsOverlay

---
+++ Engine/StatsOverlay.cs
@@ -0,0 +1,36 @@
+﻿using FRAMEDRAG.Engine.Display;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FRAMEDRAG.Engine
+{
+    public class StatsOverlay : Component
+    {
+
+        public Container Container = new Container();
+        public StatsOverlay(EngineGame engine)
+            : base(engine)
+        {
+            TextOverlay = new Text(@"", engine.DefaultFont);
+            TextOverlay.Position = Vector2.Zero;
+            engine.Stage.AddChild(TextOverlay);
+        }
+
+        public Text TextOverlay;
+
+        public double FPS = 0f;
+        public override void Update(GameTime gameTime)
+        {
+            FPS = 1f / gameTime.ElapsedGameTime.TotalSeconds;
+        }
+
+        public override void FixedUpdate(GameTime gameTime)
+        {
+            TextOverlay.SetText($@"FPS: {Math.Round(FPS, 2)}");
+        }
+    }
+}
