19bb518324fbef82e7225566c4c2f3e1e04473c2
kate <kate@dariox.club>
2022-06-22 19:38:23 +0800

    Set window size to 720p

--- FRAMEDRAG.DVDExample/DVDGame.cs
+++ FRAMEDRAG.DVDExample/DVDGame.cs
@@ -11,6 +11,12 @@ namespace FRAMEDRAG.DVDExample
 {
     internal class DVDGame : EngineGame
     {
+        public DVDGame()
+            :base()
+        {
+            WindowWidth = 1280;
+            WindowHeight = 720;
+        }
         protected override void Initialize()
         {
             base.Initialize();
@@ -18,8 +24,8 @@ namespace FRAMEDRAG.DVDExample
             LoadImage(@"C:\Work\_ExampleData\garfield.jpg");
             var testTexture = new Texture(TextureCache[@"garfield"]);
             var testSprite = new DVDSprite(testTexture);
-            testSprite.Scale.X = 0.5f;
-            testSprite.Scale.Y = 0.5f;
+            testSprite.Scale.X = 0.1f;
+            testSprite.Scale.Y = 0.1f;
             Stage.AddChild(testSprite);
         }
     }
