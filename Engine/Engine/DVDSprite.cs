0362ce2a8074137bfa7002b714fa5623f6c4314f
kate <kate@dariox.club>
2022-06-22 16:28:03 +0800

    Create class DVDSprite

---
+++ Engine/Engine/DVDSprite.cs
@@ -0,0 +1,34 @@
+﻿using Engine.Display;
+using Engine.Textures;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    internal class DVDSprite : Sprite
+    {
+        public DVDSprite(Texture texture) : base(texture)
+        {
+        }
+
+        private Vector2 change = new Vector2(1f, 1f);
+        public override void Update(GameTime gameTime, EngineGame engine)
+        {
+            if (Position.X <= 1)
+                change.X = 1f;
+            else if (Position.X + Width >= engine.graphicsDevice.PreferredBackBufferWidth)
+                change.X = -1f;
+
+            if (Position.Y <= 1)
+                change.Y = 1f;
+            else if (Position.Y + Height >= engine.graphicsDevice.PreferredBackBufferHeight)
+                change.Y = -1f;
+
+            Position += change;
+        }
+    }
+}
