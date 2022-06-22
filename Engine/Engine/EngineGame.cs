8d7b057021d40ed1eba4a492006bbaa06a851161
kate <kate@dariox.club>
2022-06-22 12:33:44 +0800

    Update Engine.sln, Engine.csproj, and 2 more files...

---
+++ Engine/Engine/EngineGame.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class EngineGame : Game
+    {
+        public GraphicsDeviceManager graphicsDevice;
+        public SpriteBatch spriteBatch;
+
+        public double FixedUpdateTime = 1f / 30f;
+        public double FixedFastUpdateTime = 1f / 500f;
+
+        public double TargetFramerate = 1000f;
+        public float MouseSensitivity = 6f;
+
+        public Dictionary<string, object> AssetTable;
+
+        public EngineGame()
+        {
+            graphicsDevice = new GraphicsDeviceManager(this);
+
+            graphicsDevice.PreferredBackBufferHeight = 720;
+            graphicsDevice.PreferredBackBufferWidth = 1280;
+            graphicsDevice.HardwareModeSwitch = false;
+            graphicsDevice.GraphicsProfile = GraphicsProfile.HiDef;
+            graphicsDevice.SynchronizeWithVerticalRetrace = true;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            checkFixedUpdate(gameTime);
+        }
+        protected override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+
+        // FixedUpdate related
+        private double fixedUpdateTimer = 0;
+        private void checkFixedUpdate(GameTime gameTime)
+        {
+            while (fixedUpdateTimer >= FixedUpdateTime)
+            {
+                FixedUpdate(gameTime);
+                fixedUpdateTimer -= FixedUpdateTime;
+            }
+        }
+        protected void FixedUpdate(GameTime gameTime)
+        { }
+        // FixedFastUpdate related
+        private double fixedfastUpdateTimer = 0;
+        private void checkFixedFastUpdate(GameTime gameTime)
+        {
+            while (fixedfastUpdateTimer >= FixedFastUpdateTime)
+            {
+                FixedFastUpdate(gameTime);
+                fixedfastUpdateTimer -= FixedFastUpdateTime;
+            }
+        }
+        protected void FixedFastUpdate(GameTime gameTime)
+        { }
+    }
+}
---
+++ Engine/Engine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Engine
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(@"cunt");
+        }
+    }
+}
