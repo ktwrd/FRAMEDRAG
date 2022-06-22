fff672d97ff3697b328cef1a6ac5f03c14eb8bb9
kate <kate@dariox.club>
2022-06-22 21:20:59 +0800

    Add Minimum and Maximum FPS

--- Engine/StatsOverlay.cs
+++ Engine/StatsOverlay.cs
@@ -1,5 +1,6 @@
 ﻿using FRAMEDRAG.Engine.Display;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,14 +24,34 @@ namespace FRAMEDRAG.Engine
         public Text TextOverlay;
 
         public double FPS = 0f;
+        public double FPS_Max = 0f;
+        public double FPS_Min = double.MaxValue;
         public override void Update(GameTime gameTime)
         {
-            FPS = 1f / gameTime.ElapsedGameTime.TotalSeconds;
+            FPS = 1000.0f / gameTime.ElapsedGameTime.TotalMilliseconds;
+            if (FPS == double.PositiveInfinity || FPS == double.NegativeInfinity) return;
+            if (FPS_Min > FPS)
+                FPS_Min = FPS;
+            if (FPS_Max < FPS)
+                FPS_Max = FPS;
+            if (Keyboard.GetState().IsKeyDown(Keys.Tab))
+                ResetFPSValues();
+        }
+        private void ResetFPSValues()
+        {
+            FPS_Max = 0f;
+            FPS_Min = double.MaxValue;
         }
 
         public override void FixedUpdate(GameTime gameTime)
         {
-            TextOverlay.SetText($@"FPS: {Math.Round(FPS, 2)}");
+            string[] lines = new string[]
+            {
+                $"FPS      : {Math.Round(FPS, 2).ToString().PadLeft(10)}",
+                $"FPS (Max): {Math.Round(FPS_Max, 2).ToString().PadLeft(10)}",
+                $"FPS (Min): {Math.Round(FPS_Min, 2).ToString().PadLeft(10)}",
+            };
+            TextOverlay.SetText(string.Join("\n", lines));
         }
     }
 }
