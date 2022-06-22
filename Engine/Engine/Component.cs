dfb5a6e12015d89d394f0f69c822e2e36150925a
kate <kate@dariox.club>
2022-06-22 16:40:47 +0800

    Create class Component

---
+++ Engine/Engine/Component.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Component : GameComponent
+    {
+        public Component(EngineGame game)
+            : base(game)
+        {
+            Game = game;
+        }
+        public new EngineGame Game;
+        public virtual void Draw(GameTime gameTime)
+        { }
+        public virtual void FixedUpdate(GameTime gameTime)
+        { }
+        public virtual void FixedFastUpdate(GameTime gameTime)
+        { }
+    }
+}
