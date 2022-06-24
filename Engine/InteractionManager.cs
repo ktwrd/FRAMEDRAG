56658550c44246b43e1b9719496d0456dad1c1f3
kate <kate@dariox.club>
2022-06-24 20:16:19 +0800

    Create class InteractionManager

---
+++ Engine/InteractionManager.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FRAMEDRAG.Engine
+{
+    public class InteractionManager : Component
+    {
+        private EngineGame Engine;
+        public InteractionManager(EngineGame engine)
+            : base(engine)
+        {
+            Engine = engine;
+        }
+
+        public KeyboardState? KeyboardState;
+        public MouseState? MouseState;
+        public override void Update(GameTime gameTime)
+        {
+            UpdateKeyboardState();
+            UpdateMouseState();
+        }
+        private void UpdateKeyboardState()
+        {
+            var kstate = Keyboard.GetState();
+            if (KeyboardState != null)
+            {
+
+            }
+            KeyboardState = kstate;
+        }
+        private void UpdateMouseState()
+        {
+            var mstate = Mouse.GetState();
+            if (MouseState != null)
+            {
+
+            }
+            MouseState = mstate;
+        }
+    }
+}
