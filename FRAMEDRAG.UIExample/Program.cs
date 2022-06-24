81f3e614c544a6fbfbbc5963833cfe9a1c818a00
kate <kate@dariox.club>
2022-06-24 14:18:58 +0800

    Add UIGame class

--- FRAMEDRAG.UIExample/Program.cs
+++ FRAMEDRAG.UIExample/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FRAMEDRAG.UIExample;
+
+var game = new UIGame();
+game.Run();
---
+++ FRAMEDRAG.UIExample/UIGame.cs
@@ -0,0 +1,19 @@
+﻿using FRAMEDRAG.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FRAMEDRAG.UIExample
+{
+    internal class UIGame : EngineGame
+    {
+        internal UIGame()
+            : base()
+        {
+            WindowWidth = 1280;
+            WindowHeight = 720;
+        }
+    }
+}
