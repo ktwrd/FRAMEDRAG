eaebd4ba88f79dde23ac65e2448f882fe73432e7
kate <kate@dariox.club>
2022-06-24 20:16:13 +0800

    Create class GeneralHelper

---
+++ Engine/Helpers/GeneralHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FRAMEDRAG.Engine.Helpers
+{
+    public static class GeneralHelper
+    {
+        public static byte[] StreamToByteArray(Stream input)
+        {
+            MemoryStream ms = new MemoryStream();
+            input.CopyTo(ms);
+            return ms.ToArray();
+        }
+    }
+}
