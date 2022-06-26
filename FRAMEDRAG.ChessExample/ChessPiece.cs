6ec57f38b7669c1bead2adfac3bb3f79921cf12f
kate <kate@dariox.club>
2022-06-26 13:22:55 +0800

    Create class ChessPiece

---
+++ FRAMEDRAG.ChessExample/ChessPiece.cs
@@ -0,0 +1,19 @@
+﻿using FRAMEDRAG.Engine;
+using FRAMEDRAG.Engine.Display;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FRAMEDRAG.ChessExample
+{
+    public class ChessPiece : Component
+    {
+        public Container PieceContainer;
+        public ChessPiece(ChessGame engine) : base(engine)
+        {
+
+        }
+    }
+}
