9129acc66d50dcacc8482564f8bb4d61fc86c53a
kate <kate@dariox.club>
2022-06-26 12:10:33 +0800

    Create class ChessBoard

---
+++ FRAMEDRAG.ChessExample/ChessBoard.cs
@@ -0,0 +1,33 @@
+﻿using FRAMEDRAG.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FRAMEDRAG.ChessExample
+{
+    public enum Piece
+    {
+        Unknown = -1,
+        Pawn,
+        Bishop,
+        Knight,
+        Rook,
+        Queen,
+        King
+    }
+    public enum PieceTeam
+    {
+        Unknown = -1,
+        White,
+        Black
+    }
+
+    public class ChessBoard : Component
+    {
+        public ChessBoard(EngineGame engine) : base(engine)
+        {
+        }
+    }
+}
