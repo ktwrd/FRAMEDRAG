476d9452472c5060b1b564f1133fff96c014d5d3
kate <kate@dariox.club>
2022-08-19 11:51:09 +0800

    Add method ToGlobal

--- FRAMEDRAG.Engine.Shared/Display/DisplayObject.cs
+++ FRAMEDRAG.Engine.Shared/Display/DisplayObject.cs
@@ -64,6 +64,15 @@ namespace FRAMEDRAG.Engine.Display
         {
             return (pos - GlobalPosition()) / AbsoluteScale();
         }
+        public Vector2 ToGlobal(Vector2 pos)
+        {
+            var newPosition = new Vector2();
+
+            newPosition.X = (WorldTransform[0] * pos.X) + (WorldTransform[2] * pos.Y) + WorldTransform[4];
+            newPosition.Y = (WorldTransform[1] * pos.X) + (WorldTransform[3] * pos.Y) + WorldTransform[5];
+
+            return newPosition;
+        }
         /*private Graphics mask;
         public Graphics Mask
         {
@@ -97,24 +106,22 @@ namespace FRAMEDRAG.Engine.Display
                 _cr = (float)Math.Cos(Rotation);
             }
 
-            var localTransform = LocalTransform;
             var parentTransform = Parent.WorldTransform;
-            var worldTransform = WorldTransform;
 
-            localTransform[0] = _cr * Scale.X;
-            localTransform[1] = -_sr * Scale.Y;
-            localTransform[3] = _sr * Scale.X;
-            localTransform[4] = _cr * Scale.Y;
+            LocalTransform[0] = _cr * Scale.X; // a
+            LocalTransform[1] = -_sr * Scale.Y;// b
+            LocalTransform[3] = _sr * Scale.X; // d
+            LocalTransform[4] = _cr * Scale.Y; // tx
 
             var px = Pivot.X;
             var py = Pivot.Y;
 
-            var a00 = localTransform[0];
-            var a01 = localTransform[1];
-            var a02 = Position.X - localTransform[0] * px - py * localTransform[1];
-            var a10 = localTransform[3];
-            var a11 = localTransform[4];
-            var a12 = Position.Y - localTransform[4] * py - px * localTransform[3];
+            var a00 = LocalTransform[0];
+            var a01 = LocalTransform[1];
+            var a02 = Position.X - LocalTransform[0] * px - py * LocalTransform[1];
+            var a10 = LocalTransform[3];
+            var a11 = LocalTransform[4];
+            var a12 = Position.Y - LocalTransform[4] * py - px * LocalTransform[3];
 
             var b00 = parentTransform[0];
             var b01 = parentTransform[1];
@@ -123,16 +130,16 @@ namespace FRAMEDRAG.Engine.Display
             var b11 = parentTransform[4];
             var b12 = parentTransform[5];
 
-            localTransform[2] = a02;
-            localTransform[5] = a12;
+            LocalTransform[2] = a02; // c
+            LocalTransform[5] = a12; // ty
 
-            worldTransform[0] = b00 * a00 + b01 * a10;
-            worldTransform[1] = b00 * a01 + b01 * a11;
-            worldTransform[2] = b00 * a02 + b01 * a12 + b02;
+            WorldTransform[0] = b00 * a00 + b01 * a10;
+            WorldTransform[1] = b00 * a01 + b01 * a11;
+            WorldTransform[2] = b00 * a02 + b01 * a12 + b02;
 
-            worldTransform[3] = b10 * a00 + b11 * a10;
-            worldTransform[4] = b10 * a01 + b11 * a11;
-            worldTransform[5] = b10 * a02 + b11 * a12 + b12;
+            WorldTransform[3] = b10 * a00 + b11 * a10;
+            WorldTransform[4] = b10 * a01 + b11 * a11;
+            WorldTransform[5] = b10 * a02 + b11 * a12 + b12;
 
             WorldAlpha = Alpha * Parent.WorldAlpha;
 
