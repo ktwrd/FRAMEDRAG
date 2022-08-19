9983ef77853d4cafcaeddc26ccc64ea8baf535fa
kate <kate@dariox.club>
2022-08-19 11:50:24 +0800

    Add Events for Keyboard and Mouse

--- FRAMEDRAG.Engine.Shared/InteractionManager.cs
+++ FRAMEDRAG.Engine.Shared/InteractionManager.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using FRAMEDRAG.Engine.Helpers;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
@@ -19,17 +20,35 @@ namespace FRAMEDRAG.Engine
 
         public KeyboardState? KeyboardState;
         public MouseState? MouseState;
-        public override void Update(GameTime gameTime)
+        public override void FixedFastUpdate(GameTime gameTime)
         {
-            UpdateKeyboardState();
-            UpdateMouseState();
+            var tarr = new Task[]
+            {
+                new Task(delegate{UpdateKeyboardState(); }),
+                new Task(delegate{UpdateMouseState(); }),
+            };
+            foreach (var i in tarr)
+                i.Start();
+            Task.WhenAll(tarr).Wait();
+            base.FixedFastUpdate(gameTime);
         }
+
+        public new event MouseStateDelegate MouseUp;
+        public new event MouseStateDelegate MouseDown;
+        public event MouseStateDelegate MousePress;
+        public event KeyboardStateDelegate KeyUp;
+        public event KeyboardStateDelegate KeyDown;
+
         private void UpdateKeyboardState()
         {
             var kstate = Keyboard.GetState();
             if (KeyboardState != null)
             {
-
+                var pressed = kstate.GetPressedKeys();
+                KeyDown?.Invoke(kstate, pressed);
+                Keys[] missingKeys = KeyboardState?.GetPressedKeys().Where((v) => !pressed.Contains(v)).ToArray() ?? Array.Empty<Keys>();
+                if (missingKeys.Length > 0)
+                    KeyUp?.Invoke(kstate, missingKeys);
             }
             KeyboardState = kstate;
         }
@@ -38,7 +57,63 @@ namespace FRAMEDRAG.Engine
             var mstate = Mouse.GetState();
             if (MouseState != null)
             {
-
+                bool InvokedUp = false;
+                bool InvokedDown = false;
+                bool InvokedPress = false;
+                if (mstate.LeftButton == ButtonState.Released && MouseState?.LeftButton == ButtonState.Pressed && !InvokedUp)
+                {
+                    MouseUp?.Invoke(mstate);
+                    InvokedUp = true;
+                }
+                if (mstate.LeftButton == ButtonState.Pressed)
+                {
+                    if (MouseState?.LeftButton == ButtonState.Released && !InvokedPress)
+                    {
+                        MousePress?.Invoke(mstate);
+                        InvokedPress = true;
+                    }
+                    if (!InvokedDown)
+                    {
+                        MouseDown?.Invoke(mstate);
+                        InvokedDown = true;
+                    }
+                }
+                if (mstate.MiddleButton == ButtonState.Released && MouseState?.MiddleButton == ButtonState.Pressed && !InvokedUp)
+                {
+                    MouseUp?.Invoke(mstate);
+                    InvokedUp = true;
+                }
+                if (mstate.MiddleButton == ButtonState.Pressed)
+                {
+                    if (MouseState?.MiddleButton == ButtonState.Released && !InvokedPress)
+                    {
+                        MousePress?.Invoke(mstate);
+                        InvokedPress = true;
+                    }
+                    if (!InvokedDown)
+                    {
+                        MouseDown?.Invoke(mstate);
+                        InvokedDown = true;
+                    }
+                }
+                if (mstate.RightButton == ButtonState.Released && MouseState?.RightButton == ButtonState.Pressed && !InvokedUp)
+                {
+                    MouseUp?.Invoke(mstate);
+                    InvokedUp = true;
+                }
+                if (mstate.RightButton == ButtonState.Pressed)
+                {
+                    if (MouseState?.RightButton == ButtonState.Released && !InvokedPress)
+                    {
+                        MousePress?.Invoke(mstate);
+                        InvokedPress = true;
+                    }
+                    if (!InvokedDown)
+                    {
+                        MouseDown?.Invoke(mstate);
+                        InvokedDown = true;
+                    }
+                }
             }
             MouseState = mstate;
         }
