eab65f1cbba00b188ff32f20515e0e887b235035
kate <kate@dariox.club>
2022-08-19 11:50:05 +0800

    Create class ViewportEngineGame

---
+++ FRAMEDRAG.Engine.Shared/ViewportEngineGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FRAMEDRAG.Engine
+{
+    public class ViewportEngineGame : EngineGame
+    {
+        public ViewportContainer Viewport;
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+            Viewport = new ViewportContainer(this);
+        }
+    }
+}
