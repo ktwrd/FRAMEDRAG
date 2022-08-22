47339781c6c61d7ef2541565190ddf4b1119ee1a
kate <kate@dariox.club>
2022-08-22 09:51:18 +0800

    Update ViewportContainer.cs

--- FRAMEDRAG.Engine.Shared/ViewportContainer.cs
+++ FRAMEDRAG.Engine.Shared/ViewportContainer.cs
@@ -32,6 +32,7 @@ namespace FRAMEDRAG.Engine
 
         public int ScreenWidth => Game.graphicsDevice.PreferredBackBufferWidth;
         public int ScreenHeight => Game.graphicsDevice.PreferredBackBufferHeight;
+        public Vector2 ScreenSize => new Vector2(Game.graphicsDevice.PreferredBackBufferWidth, Game.graphicsDevice.PreferredBackBufferHeight);
 
         public override void Update(GameTime gameTime)
         {
@@ -72,6 +73,50 @@ namespace FRAMEDRAG.Engine
         public double WorldWidth => Container.GetBounds().Width / Container.Scale.X;
         public double WorldHeight => Container.GetBounds().Height / Container.Scale.Y;
 
+        public Vector2 ToWorld(Vector2 pos) => Container.GlobalToLocal(pos);
 
+        public double WorldScreenWidth => WorldWidth * Container.Scale.X;
+        public double WorldScreenHeight => WorldHeight * Container.Scale.Y;
+        public Vector2 WorldScreen => new Vector2(Convert.ToSingle(WorldScreenWidth), Convert.ToSingle(WorldScreenHeight));
+        public Vector2 CenterPosition => new Vector2(
+            Convert.ToSingle((WorldScreenWidth / 2f) - (Container.Position.X / Container.Scale.X)),
+            Convert.ToSingle((WorldScreenHeight / 2f) - (Container.Position.Y / Container.Scale.Y)));
+
+        public void MoveCenter(Vector2 position)
+        {
+            var newPosition = ((WorldScreen / 2f) - position) * Container.Scale;
+
+            if (Container.Position != newPosition)
+            {
+                Container.Position = newPosition;
+            }
+        }
+        public void MoveCorner(Vector2 position)
+        {
+            var newPosition = position * Container.Scale;
+            if (Container.Position != newPosition)
+            {
+                Container.Position = newPosition;
+            }
+        }
+
+        public double ScreenWidthInWorldPixels => ScreenWidth / Container.Scale.X;
+        public double ScreenHeightInWorldPixels => ScreenHeight / Container.Scale.Y;
+        public Vector2 ScreenInWorldPixels => new Vector2(
+            (float)ScreenWidthInWorldPixels,
+            (float)ScreenHeightInWorldPixels);
+
+        public double FindFitWidth(double width) => ScreenWidth / width;
+        public double FindFitHeight(double height) => ScreenHeight / height;
+        public double FindFitVec(Vector2 pos)
+        {
+            var res = ScreenSize / pos;
+            return Math.Min(res.X, res.Y);
+        }
+        public double FindConverVec(Vector2 pos)
+        {
+            var res = ScreenSize / pos;
+            return Math.Max(res.X, res.Y);
+        }
     }
 }
